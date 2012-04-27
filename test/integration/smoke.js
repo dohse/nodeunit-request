fefdfff9e694e3fe0eb3e503920edc86d19281c4
Jonas Dohse <jonas@mbr-targeting.com>
2012-04-27 14:21:59 +0200

    Remove dispensable boilerplate stuff

--- test/integration/smoke.js
+++ test/integration/smoke.js
@@ -1,15 +1,5 @@
 // Copyright 2012 Jonas Dohse. All Rights Reserved.
 
-var childProcess = require('child_process');
-
-var async = require('async');
-
-var ApiCall = require('../lib/ApiCall');
-
-exports['get /test'] = ApiCall.withApi(function(test, cb) {
-  var requests = [
-    test.request('get', '/test', null)([test.code(200)]),
-    test.request('get', '/nonexistent', null)([test.code(404)])
-  ];
-  async.series(requests, cb);
-});
+exports['Test trivial server case'] = function(test) {
+  test.done();
+};
