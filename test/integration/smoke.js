6ff934778ca714fdbc718f3fd52f79013a8c3b2f
Jonas Dohse <jonas@mbr-targeting.com>
2012-04-27 16:53:00 +0200

    Add `config` parameter and basic smoke test

--- test/integration/smoke.js
+++ test/integration/smoke.js
@@ -1,5 +1,27 @@
 // Copyright 2012 Jonas Dohse. All Rights Reserved.
 
-exports['Test trivial server case'] = function(test) {
-  test.done();
+var async = require('async');
+
+var httpTest = require('../../lib/http-test');
+
+var httpUrl = exports.url = 'http://localhost:19585';
+
+var config = {
+  url: httpUrl,
+  main: 'test/lib/express-example.js'
 };
+
+exports['Test trivial server case'] = httpTest(config, function(test, cb) {
+  var json = {
+    a: 1,
+    b: true,
+    c: 'c',
+    d: []
+  };
+  var requests = [
+    test.request('get', '/403')([test.code(403)]),
+    test.request('get', '/200')([test.code(200)]),
+    test.request('post', '/200', json)([test.code(200), test.json(json)])
+  ];
+  async.series(requests, cb);
+});
---
+++ test/lib/express-example.js
@@ -0,0 +1,21 @@
+// Copyright 2012 Jonas Dohse. All Rights Reserved.
+
+var url = require('url');
+
+var express = require('express');
+
+var smoke = require('../integration/smoke');
+
+var app = express.createServer();
+
+app.use(express.bodyParser());
+
+function reflect(req, res, next) {
+  res.header('Content-Type', req.headers['content-type']);
+  res.send(req.body, +req.params.code);
+}
+
+app.get('/:code', reflect);
+app.post('/:code', reflect);
+
+app.listen(url.parse(smoke.url).port);
